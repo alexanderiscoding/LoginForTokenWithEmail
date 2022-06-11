b9c8b3bc08e5440240d6615dc5133a5a30710fe2
alexanderfsm <94754185+alexanderfsm@users.noreply.github.com>
2022-06-11 17:21:30 -0300

    Update LoginWithPDO.php

--- LoginWithPDO.php
+++ LoginWithPDO.php
@@ -169,8 +169,7 @@
             header("Location: /bloqueado"); 
             exit();
         }
-    }
-    if ($_GET) {
+    }else if ($_GET) {
         if (ctype_xdigit($_GET["token"]) && saveAccess() && validateToken()) {
             if(getUser($_SESSION["user"])){
                 header("Location: /autorizado"); 
@@ -182,6 +181,15 @@
             }
         } else {
             header("Location: /bloqueado"); 
+            exit();
+        }
+    }else{
+        session_start();
+        if($_SESSION["user"]){
+            header("Location: /autorizado"); 
+            exit();
+        }else{
+            header("Location: /reenviar"); 
             exit();
         }
     }
